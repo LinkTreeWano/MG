dc955e6ea5da82fdf289254c7fb35352bc2ddbc4
LinkTreeWano <150118925+LinkTreeWano@users.noreply.github.com>
2023-11-08 20:05:57 +0300

    Update snowfall.js

--- snowfall.js
+++ snowfall.js
@@ -1,218 +1,201 @@
-type BlossomSceneConfig = {
-	id: string;
-	petalsTypes: Petal[];
-	numPetals ?: number;
-	gravity ?: number; //0~1
-	windMaxSpeed ?: number;
-}
+const Sakura = function(selector, options) {
+  if (typeof selector === 'undefined') {
+    throw new Error('No selector present. Define an element.');
+  }
 
-interface PetalConfig {
-	customClass?: string;
-	x?: number;
-	y?: number;
-	z?: number;
-	xSpeedVariation?: number;
-	ySpeed?: number;
-	rotation?: PetalRotation;
-}
+  this.el = document.querySelector(selector);
 
-type PetalRotation = {
-	axis: 'X'|'Y'|'Z';
-	value: number;
-	speed: number;
-	x: number;
-}
+  // Defaults for the option object, which gets extended below.
+  const defaults = {
+    className: 'sakura', // Classname of the petal. This corresponds with the css.
+    fallSpeed: 1, // Speed factor in which the petal falls (higher is slower).
+    maxSize: 14, // The maximum size of the petal.
+    minSize: 10, // The minimum size of the petal.
+    delay: 300, // Delay between petals.
+    colors: [
+      {
+        // You can add multiple colors (chosen randomly) by adding elements to the array.
+        gradientColorStart: 'rgba(255, 183, 197, 0.9)', // Gradient color start (rgba).
+        gradientColorEnd: 'rgba(255, 197, 208, 0.9)', // Gradient color end (rgba).
+        gradientColorDegree: 120, // Gradient degree angle.
+      },
+    ],
+  };
 
-class Petal implements PetalConfig {
-	el: HTMLElement;
+  // Merge defaults with user options.
+  const extend = function(originalObj, newObj) {
+    Object.keys(originalObj).forEach(key => {
+      if (newObj && Object.prototype.hasOwnProperty.call(newObj, key)) {
+        const origin = originalObj;
+        origin[key] = newObj[key];
+      }
+    });
 
-	constructor(config: PetalConfig) {
-		this.customClass = config.customClass || '';
-		this.x = config.x || 0;
-		this.y = config.y || 0;
-		this.z = config.z || 0; 
-		this.xSpeedVariation = config.xSpeedVariation || 0;
-		this.ySpeed = config.ySpeed || 0;
-		this.rotation = {
-			axis: 'X',
-			value: 0,
-			speed: 0,
-			x: 0
-		};
+    return originalObj;
+  };
 
-		if (config.rotation && typeof config.rotation === 'object') {
-			this.rotation.axis = config.rotation.axis || this.rotation.axis;
-			this.rotation.value = config.rotation.value || this.rotation.value;
-			this.rotation.speed = config.rotation.speed || this.rotation.speed;
-			this.rotation.x = config.rotation.x || this.rotation.x;
-		}
+  this.settings = extend(defaults, options);
 
-		this.el = document.createElement('div');
-		this.el.className = 'petal  ' + this.customClass;
-		this.el.style.position = 'absolute';
-		this.el.style.backfaceVisibility = 'visible';
-	}
-}
+  // Hide horizontal scrollbars on the target element.
+  this.el.style.overflowX = 'hidden';
 
-class BlossomScene {
-	container: HTMLElement;
-	numPetals: number;
-	petalsTypes: Petal[];
-	gravity: number;
-	windMaxSpeed: number;
-	private windMagnitude: number;
-	private placeholder: HTMLElement;
-	private petals: Petal[];
-	private windDuration: number;
-	private width: number;
-	private height: number;
-	private timer: number;
+  // Random array element
+  function randomArrayElem(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
 
-	constructor(config: BlossomSceneConfig) {
-		let container = document.getElementById(config.id);
-		if (container === null) {
-			throw new Error('[id] provided was not found in document');
-		}
-		this.container = container;
-		this.placeholder = document.createElement('div');
-		this.petals = [];
-		this.numPetals = config.numPetals || 5;
-		this.petalsTypes = config.petalsTypes;
-		this.gravity = config.gravity || 0.8;
-		this.windMaxSpeed = config.windMaxSpeed || 4;
-		this.windMagnitude = 0.2;
-		this.windDuration = 0;
-		this.width = this.container.offsetWidth;
-		this.height = this.container.offsetHeight;
-		this.timer = 0;
+  // Random integer
+  function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
-		this.container.style.overflow = 'hidden';
-		this.placeholder.style.transformStyle = 'preserve-3d';
-		this.placeholder.style.width = this.container.offsetWidth + 'px';
-		this.placeholder.style.height = this.container.offsetHeight + 'px';
-		this.container.appendChild(this.placeholder);
-		this.createPetals();
-		requestAnimationFrame(this.updateFrame.bind(this));
-	}
+  // Check for animation events.
+  const prefixes = ['webkit', 'moz', 'MS', 'o', ''];
+  function PrefixedEvent(element, type, callback) {
+    for (let p = 0; p < prefixes.length; p += 1) {
+      let animType = type;
 
-	/**
-	 * Reset the petal position when it goes out of container
-	 */
-	resetPetal(petal: Petal) {
-		petal.x = this.width * 2 - Math.random() * this.width * 1.75;
-		petal.y = petal.el.offsetHeight * -1;
-		petal.z = Math.random() * 200;
+      if (!prefixes[p]) {
+        animType = type.toLowerCase();
+      }
 
-		if (petal.x > this.width) {
-			petal.x = this.width + petal.el.offsetWidth;
-			petal.y = Math.random() * this.height / 2;
-		}
+      element.addEventListener(prefixes[p] + animType, callback, false);
+    }
+  }
 
-		// Rotation
-		petal.rotation.speed = Math.random() * 10;
-		let randomAxis = Math.random();
-		if (randomAxis > 0.5) {
-			petal.rotation.axis = 'X';
-		} else if (randomAxis > 0.25) {
-			petal.rotation.axis = 'Y';
-			petal.rotation.x = Math.random() * 180 + 90;
-		} else {
-			petal.rotation.axis = 'Z';
-			petal.rotation.x = Math.random() * 360 - 180;
-			// looks weird if the rotation is too fast around this axis
-			petal.rotation.speed = Math.random() * 3;
-		}
+  // Check if the element is in the viewport.
+  function elementInViewport(el) {
+    const rect = el.getBoundingClientRect();
 
-		// random speed
-		petal.xSpeedVariation = Math.random() * 0.8 - 0.4;
-		petal.ySpeed = Math.random() + this.gravity;
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
 
-		return petal;
-	}
+  this.createPetal = () => {
+    if (this.el.dataset.sakuraAnimId) {
+      setTimeout(() => {
+        window.requestAnimationFrame(this.createPetal);
+      }, this.settings.delay);
+    }
 
-	/**
-	 * Calculate wind speed
-	 */
-	calculateWindSpeed(t: number, y: number) {
-		let a = this.windMagnitude / 2 * (this.height - 2 * y / 3) / this.height;
-		return a * Math.sin(2 * Math.PI / this.windDuration * t + (3 * Math.PI / 2)) + a;
-	}
+    // Name the animations. These have to match the animations in the CSS file.
+    const animationNames = {
+      blowAnimations: [
+        'blow-soft-left',
+        'blow-medium-left',
+        'blow-soft-right',
+        'blow-medium-right',
+      ],
+      swayAnimations: [
+        'sway-0',
+        'sway-1',
+        'sway-2',
+        'sway-3',
+        'sway-4',
+        'sway-5',
+        'sway-6',
+        'sway-7',
+        'sway-8',
+      ],
+    };
 
-	/**
-	 * Update petal position
-	 */
-	updatePetal(petal: Petal) {
-		let petalWindSpeed = this.calculateWindSpeed(this.timer, petal.y);
-		let xSpeed = petalWindSpeed + petal.xSpeedVariation;
+    // Get one random animation of each type and randomize fall time of the petals
+    const blowAnimation = randomArrayElem(animationNames.blowAnimations);
+    const swayAnimation = randomArrayElem(animationNames.swayAnimations);
+    const fallTime =
+      (document.documentElement.clientHeight * 0.007 +
+        Math.round(Math.random() * 5)) *
+      this.settings.fallSpeed;
 
-		petal.x -= xSpeed;
-		petal.y += petal.ySpeed;
-		petal.rotation.value += petal.rotation.speed;
+    // Create animations
+    const animationsArr = [
+      `fall ${fallTime}s linear 0s 1`,
+      `${blowAnimation} ${(fallTime > 30 ? fallTime : 30) -
+        20 +
+        randomInt(0, 20)}s linear 0s infinite`,
+      `${swayAnimation} ${randomInt(2, 4)}s linear 0s infinite`,
+    ];
+    const animations = animationsArr.join(', ');
 
-		let t = 'translateX( ' + petal.x + 'px ) translateY( ' + petal.y + 'px ) translateZ( ' + petal.z + 'px )  rotate' + petal.rotation.axis + '( ' + petal.rotation.value + 'deg )';
-		if (petal.rotation.axis !== 'X') {
-			t += ' rotateX(' + petal.rotation.x + 'deg)';
-		}
-		petal.el.style.transform = t;
+    // Create petal and give it a random size.
+    const petal = document.createElement('div');
+    petal.classList.add(this.settings.className);
+    const height = randomInt(this.settings.minSize, this.settings.maxSize);
+    const width = height - Math.floor(randomInt(0, this.settings.minSize) / 3);
 
-		// reset if out of view
-		if (petal.x < -10 || petal.y > this.height + 10) {
-			this.resetPetal(petal);
-		}
-	}
+    // Get a random color.
+    const color = randomArrayElem(this.settings.colors);
 
-	/**
-	 * Change the wind speed
-	 */
-	updateWind() {
-		// wind duration should be related to wind magnitude, e.g. higher windspeed means longer gust duration
-		this.windMagnitude = Math.random() * this.windMaxSpeed;
-		this.windDuration = this.windMagnitude * 50 + (Math.random() * 20 - 10);
-	}
+    petal.style.background = `linear-gradient(${color.gradientColorDegree}deg, ${color.gradientColorStart}, ${color.gradientColorEnd})`;
+    petal.style.webkitAnimation = animations;
+    petal.style.animation = animations;
+    petal.style.borderRadius = `${randomInt(
+      this.settings.maxSize,
+      this.settings.maxSize + Math.floor(Math.random() * 10),
+    )}px ${randomInt(1, Math.floor(width / 4))}px`;
+    petal.style.height = `${height}px`;
+    petal.style.left = `${Math.random() * document.documentElement.clientWidth -
+      100}px`;
+    petal.style.marginTop = `${-(Math.floor(Math.random() * 20) + 15)}px`;
+    petal.style.width = `${width}px`;
 
-	/**
-	 * Create the petals elements
-	 */
-	createPetals() {
-		for (let i = 0; i < this.numPetals; i++) {
-			let tmpPetalType = this.petalsTypes[Math.floor(Math.random() * (this.petalsTypes.length -1))];
-			let tmpPetal = new Petal({customClass: tmpPetalType.customClass});
+    // Remove petals of which the animation ended.
+    PrefixedEvent(petal, 'AnimationEnd', () => {
+      if (!elementInViewport(petal)) {
+        petal.remove();
+      }
+    });
 
-			this.resetPetal(tmpPetal);
-			this.petals.push(tmpPetal);
-			this.placeholder.appendChild(tmpPetal.el);
-		}
-	}
+    // Remove petals that float out of the viewport.
+    PrefixedEvent(petal, 'AnimationIteration', () => {
+      if (!elementInViewport(petal)) {
+        petal.remove();
+      }
+    });
 
-	/**
-	 * Update the animation frame
-	 */
-	updateFrame() {
-		if (this.timer === this.windDuration) {
-			this.updateWind();
-			this.timer = 0;
-		}
+    // Add the petal to the target element.
+    this.el.appendChild(petal);
+  };
 
-		let petalsLen = this.petals.length;
-		for (let i = 0; i < petalsLen; i++) {
-			this.updatePetal(this.petals[i]);
-		}
-
-		this.timer++;
-		requestAnimationFrame(this.updateFrame.bind(this));
-	}
-}
-
-const petalsTypes = [
-	new Petal({customClass: 'petal-style1'}),
-	new Petal({customClass: 'petal-style2'}),
-	new Petal({customClass: 'petal-style3'}),
-	new Petal({customClass: 'petal-style4'})
-];
-
-const myBlossomSceneConfig: BlossomSceneConfig = {
-	id: 'blossom_container',
-	petalsTypes
+  this.el.setAttribute(
+    'data-sakura-anim-id',
+    window.requestAnimationFrame(this.createPetal),
+  );
 };
 
-const myBlossomScene = new BlossomScene(myBlossomSceneConfig);
+Sakura.prototype.start = function() {
+  const animId = this.el.dataset.sakuraAnimId;
+  if (!animId) {
+    this.el.setAttribute(
+      'data-sakura-anim-id',
+      window.requestAnimationFrame(this.createPetal),
+    );
+  } else {
+    throw new Error('Sakura is already running.');
+  }
+};
+
+Sakura.prototype.stop = function(graceful = false) {
+  const animId = this.el.dataset.sakuraAnimId;
+  if (animId) {
+    window.cancelAnimationFrame(animId);
+    this.el.setAttribute('data-sakura-anim-id', '');
+  }
+
+  // Remove all current blossoms at once.
+  // You can also set 'graceful' to true to stop new petals from being created.
+  // This way the petals won't be removed abruptly.
+  if (!graceful) {
+    setTimeout(() => {
+      const petals = document.getElementsByClassName(this.settings.className);
+      while (petals.length > 0) {
+        petals[0].parentNode.removeChild(petals[0]);
+      }
+    }, this.settings.delay + 50);
+  }
+};
